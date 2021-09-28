49376a2d418fcf7d43513641f511a33393ae72dd
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-09-28 01:05:25 -0500

    Bajando complejida clase tenis 1

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,10 +1,10 @@
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int scorePlayer1 = 0;
+    private int scorePlayer2 = 0;
+    private final String player1Name;
+    private final String player2Name;
 
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
@@ -12,65 +12,81 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (playerName != null && playerName.equals(this.player1Name))
+            scorePlayer1 += 1;
         else
-            m_score2 += 1;
+            scorePlayer2 += 1;
     }
 
     public String getScore() {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
+        if (scorePlayer1 == scorePlayer2)
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+            score = getString();
+            return score;
+
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+        return (scorePlayer1 >= 4 || scorePlayer2 >= 4) ? value(score) : valueFor(score,tempScore);
             }
+
+    private String getString(String score, int tempScore) {
+        switch(tempScore)
+        {
+            case 0:
+                score +="Love";
+                break;
+            case 1:
+                score +="Fifteen";
+                break;
+            case 2:
+                score +="Thirty";
+                break;
+            case 3:
+                score +="Forty";
+                break;
         }
         return score;
+    }
+
+    private String getString() {
+        String score;
+        switch (scorePlayer1)
+        {
+            case 0:
+                    score = "Love-All";
+                break;
+            case 1:
+                    score = "Fifteen-All";
+                break;
+            case 2:
+                    score = "Thirty-All";
+                break;
+            default:
+                    score = "Deuce";
+                break;
+
+        }
+        return score;
+    }
+
+    public String value(String score){
+        int minusResult = scorePlayer1 - scorePlayer2;
+        if (minusResult==1) score ="Advantage player1";
+        else if (minusResult ==-1) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
+        return score;
+    }
+
+    public String valueFor(String score,int tempScore){
+        for (int i=1; i<3; i++)
+        {
+            if (i==1) tempScore = scorePlayer1;
+            else { score+="-"; tempScore = scorePlayer2;}
+            score = getString
+                    (score, tempScore);
+        }
+    return score;
     }
 }
